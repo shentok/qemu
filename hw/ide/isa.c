85bd0eca506e09aa19325be4ac292e3480aa6168
Bernhard Beschow <shentey@gmail.com>
2022-06-12 23:07:29 +0200

    hw/ide/isa: Resolve unneeded "irq" attribute
    The irq attribute was only used temporarily in isa_ide_realizefn() and
    otherwise needlessly occupied memory.
    
    The attribute seems to be unneeded since commit
    356721aede69c80141c4a94ec26a78f91d63c9a5 ('ide: split away ide-
    internal.h').
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/ide/isa.c
+++ hw/ide/isa.c
@@ -47,7 +47,6 @@ struct ISAIDEState {
     uint32_t  iobase;
     uint32_t  iobase2;
     uint32_t  isairq;
-    qemu_irq  irq;
 };
 
 static void isa_ide_reset(DeviceState *d)
@@ -75,8 +74,7 @@ static void isa_ide_realizefn(DeviceState *dev, Error **errp)
 
     ide_bus_init(&s->bus, sizeof(s->bus), dev, 0, 2);
     ide_init_ioport(&s->bus, isadev, s->iobase, s->iobase2);
-    s->irq = isa_get_irq(isadev, s->isairq);
-    ide_init2(&s->bus, s->irq);
+    ide_init2(&s->bus, isa_get_irq(isadev, s->isairq));
     vmstate_register(VMSTATE_IF(dev), 0, &vmstate_ide_isa, s);
     ide_register_restart_cb(&s->bus);
 }
