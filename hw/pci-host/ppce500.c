b4c869d3887c6f68383907a82fe416f2ebcbf981
Bernhard Beschow <shentey@gmail.com>
2022-12-28 19:02:56 +0100

    Allocate eTSEC devices in CCSR register space

--- hw/pci-host/ppce500.c
+++ hw/pci-host/ppce500.c
@@ -16,7 +16,7 @@
 
 #include "qemu/osdep.h"
 #include "hw/irq.h"
-#include "hw/ppc/e500-ccsr.h"
+#include "hw/platform-bus.h"
 #include "hw/qdev-properties.h"
 #include "migration/vmstate.h"
 #include "hw/pci/pci_device.h"
@@ -419,11 +419,11 @@ static const VMStateDescription vmstate_ppce500_pci = {
 static void e500_pcihost_bridge_realize(PCIDevice *d, Error **errp)
 {
     PPCE500PCIBridgeState *b = PPC_E500_PCI_BRIDGE(d);
-    PPCE500CCSRState *ccsr = CCSR(container_get(qdev_get_machine(),
+    PlatformBusDevice *pbus = PLATFORM_BUS_DEVICE(container_get(qdev_get_machine(),
                                   "/e500-ccsr"));
 
-    memory_region_init_alias(&b->bar0, OBJECT(ccsr), "e500-pci-bar0", &ccsr->ccsr_space,
-                             0, int128_get64(ccsr->ccsr_space.size));
+    memory_region_init_alias(&b->bar0, OBJECT(pbus), "e500-pci-bar0", &pbus->mmio,
+                             0, int128_get64(pbus->mmio.size));
     pci_register_bar(d, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &b->bar0);
 }
 
