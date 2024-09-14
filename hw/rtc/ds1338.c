9883a2fc3521e06ec0dada08e9fb6e65ee6ec58d
Bernhard Beschow <shentey@gmail.com>
2024-09-14 17:41:55 +0200

    pericom,pt7c4338

--- hw/rtc/ds1338.c
+++ hw/rtc/ds1338.c
@@ -239,6 +239,10 @@ static const TypeInfo ds1338_types[] = {
         .instance_size = sizeof(DS1338State),
         .class_init    = ds1338_class_init,
     },
+    {
+        .name          = "pericom,pt7c4338",
+        .parent        = TYPE_DS1338,
+    },
 };
 
 DEFINE_TYPES(ds1338_types)
