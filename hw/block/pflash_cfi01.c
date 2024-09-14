shentok/qemu

commit 8f64e7449e474e18017eb1414bc13e491edd8596

hw/block/pflash_cfi01: Don't decrement pfl->counter below 0
In pflash_write() Coverity points out that we can decrement the
unsigned pfl->counter below zero, which makes it wrap around.  In
fact this is harmless, because if pfl->counter is 0 at this point we
also increment pfl->wcycle to 3, and the wcycle == 3 handling doesn't
look at counter; the only way back into code which looks at the
counter value is via wcycle == 1, which will reinitialize the counter.
But it's arguably a little clearer to break early in the "counter ==
0" if(), to avoid the decrement-below-zero.

Resolves: Coverity CID 1547611
Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
Reviewed-by: Kevin Wolf <kwolf@redhat.com>
Message-ID: <20240731143617.3391947-4-peter.maydell@linaro.org>
Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

hw/block/pflash_cfi01.c
@@ -614,6 +614,7 @@ static void pflash_write(PFlashCFI01 *pfl, hwaddr offset,
             if (!pfl->counter) {
                 trace_pflash_write(pfl->name, "block write finished");
                 pfl->wcycle++;
+                break;
             }
 
             pfl->counter--;

commit e0b9be8aee2461e9cdf547198741607b4254071f

hw/block/pflash_cfi01: Simplify control flow
Fixes: 8f64e7449e47 "hw/block/pflash_cfi01: Don't decrement pfl->counter below 0"

hw/block/pflash_cfi01.c
@@ -611,13 +611,13 @@ static void pflash_write(PFlashCFI01 *pfl, hwaddr offset,
 
             pfl->status |= 0x80;
 
-            if (!pfl->counter) {
+            if (pfl->counter) {
+                pfl->counter--;
+            } else {
                 trace_pflash_write(pfl->name, "block write finished");
                 pfl->wcycle++;
-                break;
             }
 
-            pfl->counter--;
             break;
         default:
             goto error_flash;