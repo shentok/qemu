908ec0f6d4bd9342fff65102e73c6aff4659dd77
Bernhard Beschow <shentey@gmail.com>
2024-12-20 15:34:06 +0100

    hw/sd/sdhci: Always set SDHC_CLOCK_INT_EN in usdhc_write()
    When U-Boot's driver sets the eSDHC/uSDHC controller's clock, it checks
    that the internal clock stabilizes by polling PRSSTAT_SDSTB, else it
    fails with "Internal clock never stabilised". QEMU's esdhc_read() has a
    dedicated quirk for this flag which mirrors SDHC_CLOCK_INT_STABLE which
    in turn mirrors SDHC_CLOCK_INT_EN. In the imx8mp and imx6 datasheets,
    the bit at SDHC_CLOCK_INT_EN is reserved and documented as "Always write
    as 1" but U-Boot still clears it and expects PRSSTAT_SDSTB to be set
    afterwards. Assume that this bit auto-sets itself and therefore always
    set it which fixes above error message to be triggered in QEMU in the
    uSDHC case.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/sd/sdhci.c
+++ hw/sd/sdhci.c
@@ -2098,6 +2098,11 @@ usdhc_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
         sdhci_write(opaque, offset, val | s->trnmod, size);
         break;
 
+    case SDHC_CLKCON:
+        value |= SDHC_CLOCK_INT_EN;
+        sdhci_write(opaque, offset, value, size);
+        break;
+
     default:
         esdhc_write(opaque, offset, val, size);
         break;
