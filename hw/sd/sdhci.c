c081ab8e87ffde00415fa652fe632db6a2557193
Bernhard Beschow <shentey@gmail.com>
2024-12-20 15:34:06 +0100

    hw/sd/sdhci: Always set SDHC_CLOCK_SDCLK_EN in usdhc_write()
    Even though SDHC_CLOCK_SDCLK_EN is reserved and marked as "Always write
    as 1" as per e.g. imx6 and imx8mp reference manual,
    Resolves:
    
    while (!(esdhc_read32(&regs->irqstat) & flags)) {
    
    allows commands to be sent
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/sd/sdhci.c
+++ hw/sd/sdhci.c
@@ -2099,7 +2099,7 @@ usdhc_write(void *opaque, hwaddr offset, uint64_t val, unsigned size)
         break;
 
     case SDHC_CLKCON:
-        value |= SDHC_CLOCK_INT_EN;
+        value |= SDHC_CLOCK_SDCLK_EN | SDHC_CLOCK_INT_EN;
         sdhci_write(opaque, offset, value, size);
         break;
 
