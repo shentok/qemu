9b31c0547287ad89e1940ffb1fc592292d6582f5
Bernhard Beschow <shentey@gmail.com>
2025-05-08 12:29:00 +0200

    silence

--- system/memory.c
+++ system/memory.c
@@ -440,7 +440,7 @@ static MemTxResult  memory_region_read_accessor(MemoryRegion *mr,
         trace_memory_region_subpage_read(get_cpu_index(), mr, addr, tmp, size);
     } else if (trace_event_get_state_backends(TRACE_MEMORY_REGION_OPS_READ)) {
         hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
-        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
+        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0  && strcmp(memory_region_name(mr), "fsl-imx8mp-ddr-phy") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
             trace_memory_region_ops_read(get_cpu_index(), mr, abs_addr, tmp, size,
                                          memory_region_name(mr));
         }
@@ -465,7 +465,7 @@ static MemTxResult memory_region_read_with_attrs_accessor(MemoryRegion *mr,
         trace_memory_region_subpage_read(get_cpu_index(), mr, addr, tmp, size);
     } else if (trace_event_get_state_backends(TRACE_MEMORY_REGION_OPS_READ)) {
         hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
-        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
+        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0  && strcmp(memory_region_name(mr), "fsl-imx8mp-ddr-phy") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
             trace_memory_region_ops_read(get_cpu_index(), mr, abs_addr, tmp, size,
                                          memory_region_name(mr));
         }
@@ -488,7 +488,7 @@ static MemTxResult memory_region_write_accessor(MemoryRegion *mr,
         trace_memory_region_subpage_write(get_cpu_index(), mr, addr, tmp, size);
     } else if (trace_event_get_state_backends(TRACE_MEMORY_REGION_OPS_WRITE)) {
         hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
-        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
+        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0  && strcmp(memory_region_name(mr), "fsl-imx8mp-ddr-phy") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
             trace_memory_region_ops_write(get_cpu_index(), mr, abs_addr, tmp, size,
                                           memory_region_name(mr));
         }
@@ -511,7 +511,7 @@ static MemTxResult memory_region_write_with_attrs_accessor(MemoryRegion *mr,
         trace_memory_region_subpage_write(get_cpu_index(), mr, addr, tmp, size);
     } else if (trace_event_get_state_backends(TRACE_MEMORY_REGION_OPS_WRITE)) {
         hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
-        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
+        if (strcmp(memory_region_name(mr), "imx.serial") != 0 && strcmp(memory_region_name(mr), "ana_tsensor") != 0  && strcmp(memory_region_name(mr), "fsl-imx8mp-ddr-phy") != 0 && strcmp(memory_region_name(mr), "sdhci") != 0 && strcmp(memory_region_name(mr), "imx8mp.ccm.ccm") != 0 && strcmp(memory_region_name(mr), "imx.i2c") != 0 && strcmp(memory_region_name(mr), "imx.gpio") != 0) {
             trace_memory_region_ops_write(get_cpu_index(), mr, abs_addr, tmp, size,
                                           memory_region_name(mr));
         }
