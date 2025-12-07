936d00036b52a9cee385be4548c7a0223803c9f3
Bernhard Beschow <shentey@gmail.com>
2025-12-07 20:23:16 +0100

    no else after return

--- hw/net/can/flexcan.c
+++ hw/net/can/flexcan.c
@@ -193,9 +193,9 @@ static const char *flexcan_dbg_mb_code(uint32_t mb_ctrl, char *buf)
     if (code_idx & 1) {
         g_snprintf(buf, FLEXCAN_DBG_BUF_LEN, "%s+BUSY", code_str);
         return buf;
-    } else {
-        return code_str;
     }
+
+    return code_str;
 }
 
 static const char *flexcan_dbg_reg_name_fixed(hwaddr addr)
@@ -291,9 +291,9 @@ static FlexcanRegsMessageBuffer *flexcan_get_first_message_buffer(
     if (s->regs.mcr & FLEXCAN_MCR_FEN) {
         int rffn = (s->regs.ctrl2 & FLEXCAN_CTRL2_RFFN(UINT32_MAX)) >> 24;
         return s->regs.mbs + 8 + 2 * rffn;
-    } else {
-        return s->regs.mbs;
     }
+
+    return s->regs.mbs;
 }
 
 /**
@@ -1038,15 +1038,19 @@ static int flexcan_mb_rx_check_mb(FlexcanState *s, const qemu_can_frame *buf,
                                      is_serviced, is_locked);
     }
 
-    if (is_matched && is_free_to_receive && !is_locked) {
-        return FLEXCAN_CHECK_MB_MATCH;
-    } else if (is_matched && !is_locked) {
-        return FLEXCAN_CHECK_MB_MATCH_NON_FREE;
-    } else if (is_matched) {
-        return FLEXCAN_CHECK_MB_MATCH_LOCKED;
-    } else {
+    if (!is_matched) {
         return FLEXCAN_CHECK_MB_NIL;
     }
+
+    if (is_locked) {
+        return FLEXCAN_CHECK_MB_MATCH_LOCKED;
+    }
+
+    if (is_free_to_receive) {
+        return FLEXCAN_CHECK_MB_MATCH;
+    }
+
+    return FLEXCAN_CHECK_MB_MATCH_NON_FREE;
 }
 static int flexcan_mb_rx(FlexcanState *s, const qemu_can_frame *buf)
 {
@@ -1063,7 +1067,9 @@ static int flexcan_mb_rx(FlexcanState *s, const qemu_can_frame *buf)
             flexcan_mb_move_in(s, buf, mb);
             flexcan_irq_iflag_set(s, mbid);
             return FLEXCAN_RX_SEARCH_ACCEPT;
-        } else if (r == FLEXCAN_CHECK_MB_MATCH_NON_FREE) {
+        }
+
+        if (r == FLEXCAN_CHECK_MB_MATCH_NON_FREE) {
             last_not_free_to_receive_mbid = mbid;
             last_not_free_to_receive_locked = false;
         } else if (r == FLEXCAN_CHECK_MB_MATCH_LOCKED) {
@@ -1087,7 +1093,9 @@ static int flexcan_mb_rx(FlexcanState *s, const qemu_can_frame *buf)
             flexcan_mb_move_in(s, buf, &s->regs.rx_smb0);
             s->smb_target_mbidx = last_not_free_to_receive_mbid;
             return FLEXCAN_RX_SEARCH_ACCEPT;
-        } else if (s->regs.mcr & FLEXCAN_MCR_IRMQ) {
+        }
+
+        if (s->regs.mcr & FLEXCAN_MCR_IRMQ) {
             flexcan_mb_move_in(s, buf,
                                &s->regs.mbs[last_not_free_to_receive_mbid]);
             flexcan_irq_iflag_set(s, last_not_free_to_receive_mbid);
@@ -1120,7 +1128,8 @@ static ssize_t flexcan_receive(CanBusClientState *client,
         if (frame->can_id & QEMU_CAN_ERR_FLAG) {
             /* todo: error frame handling */
             continue;
-        } else if (frame->flags & QEMU_CAN_FRMF_TYPE_FD) {
+        }
+        if (frame->flags & QEMU_CAN_FRMF_TYPE_FD) {
             /* CAN FD supported only in later FlexCAN version */
             continue;
         }
