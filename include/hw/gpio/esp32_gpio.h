bdb21344d8510153c7e4eb61281f6a80a0069ddb
Bernhard Beschow <shentey@gmail.com>
2026-04-03 12:06:31 +0200

    trace++

--- include/hw/gpio/esp32_gpio.h
+++ include/hw/gpio/esp32_gpio.h
@@ -2,14 +2,11 @@
 
 #include "hw/core/sysbus.h"
 #include "hw/core/hw-error.h"
-#include "hw/core/registerfields.h"
 
 #define TYPE_ESP32_GPIO "esp32.gpio"
 #define ESP32_GPIO(obj)             OBJECT_CHECK(Esp32GpioState, (obj), TYPE_ESP32_GPIO)
 #define ESP32_GPIO_GET_CLASS(obj)   OBJECT_GET_CLASS(Esp32GpioClass, obj, TYPE_ESP32_GPIO)
 #define ESP32_GPIO_CLASS(klass)     OBJECT_CLASS_CHECK(Esp32GpioClass, klass, TYPE_ESP32_GPIO)
-
-REG32(GPIO_STRAP, 0x0038)
 
 #define ESP32_STRAP_MODE_FLASH_BOOT 0x12
 #define ESP32_STRAP_MODE_UART_BOOT  0x0f
