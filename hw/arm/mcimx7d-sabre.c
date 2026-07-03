9c40c16afe4418fceba0c69a84f8d5f0e81e3818
Bernhard Beschow <shentey@gmail.com>
2026-07-03 20:48:40 +0200

    hw/arm/fsl-imx7: Derive from TYPE_SYS_BUS_DEVICE
    Deriving from TYPE_SYS_BUS_DEVICE fixes the SoC object to be reset upon
    machine reset.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/arm/mcimx7d-sabre.c
+++ hw/arm/mcimx7d-sabre.c
@@ -52,7 +52,7 @@ static void mcimx7d_sabre_init(MachineState *machine)
     object_property_add_child(OBJECT(machine), "soc", OBJECT(s));
     object_property_set_bool(OBJECT(s), "fec2-phy-connected", false,
                              &error_fatal);
-    qdev_realize(DEVICE(s), NULL, &error_fatal);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(s), &error_fatal);
 
     memory_region_add_subregion(get_system_memory(), FSL_IMX7_MMDC_ADDR,
                                 machine->ram);
