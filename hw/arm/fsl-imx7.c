f81c0beb49b8ddd2c0de98960c79bffde98b5d31
Bernhard Beschow <shentey@gmail.com>
2026-07-03 12:24:34 +0200

    hw/timer/imx_gpt: Wire clock control module via link property
    When wiring struct IMXGPTState to the clock control module, it is
    currently necessary to reach into its private data. Moreover, when
    forgetting to wire the clock control module, QEMU will crash after the
    guest has already started. Fix both by letting struct IMXGPTState
    expose a link property which is checked at realize time.
    
    Inspired-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/arm/fsl-imx7.c
+++ hw/arm/fsl-imx7.c
@@ -250,7 +250,8 @@ static void fsl_imx7_realize(DeviceState *dev, Error **errp)
             FSL_IMX7_GPT4_IRQ,
         };
 
-        s->gpt[i].ccm = IMX_CCM(&s->ccm);
+        object_property_set_link(OBJECT(&s->gpt[i]), "clock-control-module",
+                                 OBJECT(&s->ccm), &error_abort);
         sysbus_realize(SYS_BUS_DEVICE(&s->gpt[i]), &error_abort);
         sysbus_mmio_map(SYS_BUS_DEVICE(&s->gpt[i]), 0, FSL_IMX7_GPTn_ADDR[i]);
         sysbus_connect_irq(SYS_BUS_DEVICE(&s->gpt[i]), 0,
