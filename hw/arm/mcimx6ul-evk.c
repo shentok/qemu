93e1752e460540bf867d456f099abefe98f91503
Bernhard Beschow <shentey@gmail.com>
2026-07-03 20:45:38 +0200

    hw/arm/fsl-imx6ul: Derive from TYPE_SYS_BUS_DEVICE
    Deriving from TYPE_SYS_BUS_DEVICE fixes the SoC object to be reset upon
    machine reset.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>

--- hw/arm/mcimx6ul-evk.c
+++ hw/arm/mcimx6ul-evk.c
@@ -44,7 +44,7 @@ static void mcimx6ul_evk_init(MachineState *machine)
     object_property_set_uint(OBJECT(s), "fec2-phy-num", 1, &error_fatal);
     object_property_set_bool(OBJECT(s), "fec1-phy-connected", false,
                              &error_fatal);
-    qdev_realize(DEVICE(s), NULL, &error_fatal);
+    sysbus_realize_and_unref(SYS_BUS_DEVICE(s), &error_fatal);
 
     memory_region_add_subregion(get_system_memory(), FSL_IMX6UL_MMDC_ADDR,
                                 machine->ram);
