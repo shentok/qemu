cbaa46def69ec34e3e166fd01132f205b474f489
Bernhard Beschow <shentey@gmail.com>
2026-07-18 15:00:13 +0200

    imx_ipu

--- hw/arm/fsl-imx53.c
+++ hw/arm/fsl-imx53.c
@@ -46,8 +46,7 @@ static const struct {
     [FSL_IMX53_SATA]                  = { 0x10000000, 16 * KiB, "SATA" },
     [FSL_IMX53_SATA_ALIAS]            = { 0x10004000, 64 * MiB - 16 * KiB, "SATA Aliasing" },
     [FSL_IMX53_RESERVED_1400]         = { 0x14000000, 64 * MiB, "Reserved" },
-    [FSL_IMX53_IPU_MEM]               = { 0x18000000, 96 * MiB, "IPU_MEM" },
-    [FSL_IMX53_IPU_REGS]              = { 0x1e000000, 32 * MiB, "IPU_REGS" },
+    [FSL_IMX53_IPU]                   = { 0x18000000, 128 * MiB, "IPU" },
     [FSL_IMX53_GPU2D]                 = { 0x20000000, 256 * MiB, "GPU2D" },
     [FSL_IMX53_GPU3D]                 = { 0x30000000, 256 * MiB, "GPU3D" },
 
@@ -233,7 +232,7 @@ static void fsl_imx53_init(Object *obj)
         object_initialize_child(obj, name, &s->flexcan[i], TYPE_CAN_FLEXCAN2);
     }
 
-    object_initialize_child(obj, "ipu", &s->ipu, TYPE_IMX_IPU);
+    object_initialize_child(obj, "ipu", &s->ipu, TYPE_IMX_IPUV3M);
 
     object_initialize_child(obj, "ahci", &s->sata, TYPE_SYSBUS_AHCI);
 
@@ -505,7 +504,7 @@ static void fsl_imx53_realize(DeviceState *dev, Error **errp)
      */
     sysbus_realize(SYS_BUS_DEVICE(&s->ipu), &error_abort);
     sysbus_mmio_map(SYS_BUS_DEVICE(&s->ipu), 0,
-                    fsl_imx53_memmap[FSL_IMX53_IPU_REGS].addr);
+                    fsl_imx53_memmap[FSL_IMX53_IPU].addr);
     sysbus_connect_irq(SYS_BUS_DEVICE(&s->ipu), 0,
                        qdev_get_gpio_in(gic, 11));
 
@@ -599,7 +598,7 @@ static void fsl_imx53_realize(DeviceState *dev, Error **errp)
         case FSL_IMX53_GPIO1 ... FSL_IMX53_GPIO7:
         case FSL_IMX53_GPT:
         case FSL_IMX53_I2C1 ... FSL_IMX53_I2C3:
-        case FSL_IMX53_IPU_REGS:
+        case FSL_IMX53_IPU:
         case FSL_IMX53_OCRAM:
         case FSL_IMX53_SATA:
         case FSL_IMX53_SRTC:
